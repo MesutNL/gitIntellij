8e137a9ba3cddde3d9c9b7143974ab4318002a62
mesut <mstrby20@gmail.com>
2021-08-23 17:42:48 +0200

    Versiyon 2: Not Girisi yapildi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,10 @@
 public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("PRoje 1.asamasi bitti");
+        System.out.println("Not girisi bolumu cagrildi. ");
+    }
+
+    public static void OgrenciNotEkle(){
+        System.out.println("Not girisi");
     }
 }
